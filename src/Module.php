704c72a751ffe9c3869933df3ae9e058ea5a2eec
Judzhin Miles <info@msbios.com>
2018-04-02 10:57:59 +0300

    Intermedaite commit

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\SoftServe;
+
+/**
+ * Class Module
+ * @package MSBios\SoftServe
+ */
+class Module
+{
+
+}
